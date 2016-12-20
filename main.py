a185563fb1c2ff717007e4b3668b9b5660957326
Oz Elentok <oz.elen@gmail.com>
2016-12-20 08:04:57 +0200

    Add multithreading to download process

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import argparse
 import queue
+import threading
 import re
 import api
 
 PACKAGE_WITH_VERSION_PATTERN = re.compile(r'^(.+)@(.+)$')
+NUM_OF_WORKER_THREADS = 4
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download npm packages including dependencies')
-    parser.add_argument('-f', '--packages-file', 
+    parser.add_argument('-f', '--packages-file',
                         required=True, help='File with packages to download, seperated by newlines')
-    parser.add_argument('-o', '--output-dir', 
+    parser.add_argument('-o', '--output-dir',
                         required=True, help='Output directory to download packages to')
     return parser.parse_args()
 
@@ -26,12 +28,33 @@ def create_package_queue(packages_file):
             package_queue.put((package, version))
     return package_queue
 
+def start_package_downloaders(package_queue, output_dir):
+    worker_threads = []
+    for _ in range(NUM_OF_WORKER_THREADS):
+        worker_thread = threading.Thread(target=packages_downloader, args=(package_queue, output_dir))
+        worker_thread.start()
+        worker_threads.append(worker_thread)
+    return worker_threads
+
+def stop_package_downloaders(package_queue, workers):
+    for i in range(len(workers)):
+        package_queue.put(None)
+    for i in workers:
+        i.join()
+
 def main():
     args = parse_args()
     package_queue = create_package_queue(args.packages_file)
-    while not package_queue.empty():
+    workers = start_package_downloaders(package_queue, args.output_dir)
+    package_queue.join()
+    stop_package_downloaders(package_queue, workers)
+
+def packages_downloader(package_queue, output_dir):
+    while True:
         package = package_queue.get()
-        dependencies = api.download_package(package[0], package[1], args.output_dir)
+        if package is None:
+            break
+        dependencies = api.download_package(package[0], package[1], output_dir)
         for sub_package, sub_package_version in dependencies.items():
             package_queue.put((sub_package, sub_package_version))
         package_queue.task_done()

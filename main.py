f9a6fea9a6a1df711e1dcacbcf294d0e38410f6d
Oz Elentok <oz.elen@gmail.com>
2016-12-28 22:59:05 +0200

    Refactor multi package downloader

--- main.py
+++ main.py
@@ -1,74 +1,28 @@
 import argparse
-import multiprocessing
-import re
-from packagedownloader import NpmPackageDownloader
-from npmclient import NpmClient
-import utils
 
-PACKAGE_WITH_VERSION_PATTERN = re.compile(r'^(.+)@(.+)$')
-NUM_OF_WORKERS = 4
+from multipackagedownloader import MultiPackageDownloader
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Download npm packages including dependencies')
-    parser.add_argument('-f', '--packages-file',
-                        required=True, help='File with packages to download, seperated by newlines')
-    parser.add_argument('-o', '--output-dir',
-                        required=True, help='Output directory to download packages to')
+    parser.add_argument(
+        '-f', '--packages-file', required=True,
+        help='File with packages to download, seperated by newlines')
+    parser.add_argument(
+        '-o', '--download-dir', required=True,
+        help='Output directory to download packages to')
+    parser.add_argument(
+        '-c', '--workers-count', required=True, type=int,
+        help='Number of worker processes to spawn')
     return parser.parse_args()
-
-def create_package_queue(packages_file):
-    package_queue = multiprocessing.JoinableQueue()
-    with open(packages_file, 'r') as input_file:
-        for line in input_file:
-            package = utils.normalize_package(line.strip())
-            version = None
-            pattern_match = PACKAGE_WITH_VERSION_PATTERN.match(package)
-            if pattern_match:
-                package = pattern_match.group(1)
-                version = pattern_match.group(2)
-            package_queue.put((package, version))
-    return package_queue
-
-def start_package_downloaders(package_queue, output_dir):
-    workers = []
-    for _ in range(NUM_OF_WORKERS):
-        worker = multiprocessing.Process(target=packages_downloader, args=(package_queue, output_dir))
-        worker.start()
-        workers.append(worker)
-    return workers
-
-def stop_package_downloaders(package_queue, workers):
-    for i in range(len(workers)):
-        package_queue.put(None)
-    for i in workers:
-        i.join()
 
 def main():
     args = parse_args()
-    package_queue = create_package_queue(args.packages_file)
-    workers = start_package_downloaders(package_queue, args.output_dir)
-    package_queue.join()
-    stop_package_downloaders(package_queue, workers)
+    downloader = MultiPackageDownloader(args.packages_file, args.download_dir, args.workers_count)
+    print('multi package downloader starting')
+    downloader.start()
+    downloader.wait()
+    print('multi package downloader finished')
 
-def packages_downloader(package_queue, output_dir):
-    package_downloader = NpmPackageDownloader(output_dir)
-    while True:
-        package = package_queue.get()
-        try:
-            if package is None:
-                break
-            print('Downloading {}'.format(package[0]))
-            package_info, was_downloaded = package_downloader.download_single_package(package[0], package[1])
-            if not was_downloaded:
-                continue
-            dependencies = NpmClient.get_latest_dependencies_version(package_info.get_dependencies())
-            for sub_package, sub_package_version in dependencies.items():
-                package_queue.put((sub_package, sub_package_version))
-            print('Finished {}'.format(package[0]))
-        except Exception as e:
-            print('Failed to download {}\nException: {}'.format(package[0], e))
-        finally:
-            package_queue.task_done()
 
 if __name__ == '__main__':
     main()
---
+++ multipackagedownloader.py
@@ -0,0 +1,67 @@
+import multiprocessing
+import re
+import sys
+
+from npmclient import NpmClient
+from packagedownloader import NpmPackageDownloader
+import utils
+
+class MultiPackageDownloader:
+
+    PACKAGE_WITH_VERSION_PATTERN = re.compile(r'^(.+)@(.+)$')
+
+    def __init__(self, package_list_file_path: str, download_dir: str, num_of_workers: int):
+        self._package_queue = multiprocessing.JoinableQueue()
+        self._download_dir = download_dir
+        self._num_of_workers = num_of_workers
+        self._workers = []
+
+        self._fill_package_queue(package_list_file_path)
+
+    def _fill_package_queue(self, package_list_file_path: str):
+        with open(package_list_file_path, 'r') as input_file:
+            for line in input_file:
+                package = utils.normalize_package(line.strip())
+                version = None
+                pattern_match = MultiPackageDownloader.PACKAGE_WITH_VERSION_PATTERN.match(package)
+                if pattern_match:
+                    package = pattern_match.group(1)
+                    version = pattern_match.group(2)
+                self._package_queue.put((package, version))
+
+    def start(self):
+        self._workers = []
+        for _ in range(self._num_of_workers):
+            worker = multiprocessing.Process(
+                target=MultiPackageDownloader._packages_downloader,
+                args=(self._package_queue, self._download_dir))
+            worker.start()
+            self._workers.append(worker)
+
+    def wait(self):
+        self._package_queue.join()
+        for i in range(self._num_of_workers):
+            self._package_queue.put(None)
+        for i in self._workers:
+            i.join()
+
+    @staticmethod
+    def _packages_downloader(package_queue, download_dir):
+        package_downloader = NpmPackageDownloader(download_dir)
+        while True:
+            package = package_queue.get()
+            try:
+                if package is None:
+                    break
+                print('Downloading {}'.format(package[0]))
+                package_info, was_downloaded = package_downloader.download_single_package(package[0], package[1])
+                if not was_downloaded:
+                    continue
+                dependencies = NpmClient.get_latest_dependencies_version(package_info.get_dependencies())
+                for sub_package, sub_package_version in dependencies.items():
+                    package_queue.put((sub_package, sub_package_version))
+                print('Finished {}'.format(package[0]))
+            except Exception as e:
+                print('Failed to download {}\nException: {}'.format(package[0], e), file=sys.stderr)
+            finally:
+                package_queue.task_done()
